66490e08f80f2ecee391930c36453e336a13ad23
Fabiano Pereira <fabianop971@gmail.com>
2025-06-12 10:09:31 -0300

    Feat: adjust form add product

--- lib/screens/product_form.dart
+++ lib/screens/product_form.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:my_store/models/product.dart';
 
 class ProductForm extends StatefulWidget {
   const ProductForm({super.key});
@@ -10,20 +13,67 @@ class ProductForm extends StatefulWidget {
 class _ProductFormState extends State<ProductForm> {
   final _priceFocus = FocusNode();
   final _descriptionFocus = FocusNode();
+  final _imageUrlFocus = FocusNode();
+  final _imageUrlController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  final _formData = Map<String, Object>();
 
   @override
   void dispose() {
     super.dispose();
     _priceFocus.dispose();
+    _imageUrlFocus.dispose();
+    _descriptionFocus.dispose();
+    _imageUrlFocus.removeListener(updateImage);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _imageUrlFocus.addListener(updateImage);
+  }
+
+  void updateImage() {
+    setState(() {});
+  }
+
+  bool isValidImageUrl(String url) {
+    final isValidUrl = Uri.tryParse(url)?.hasAbsolutePath ?? false;
+    final finishWithType =
+        url.toLowerCase().endsWith(".png") ||
+        url.toLowerCase().endsWith(".jpg");
+
+    return isValidUrl && finishWithType;
+  }
+
+  void _formSave() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    _formKey.currentState?.save();
+    final newProduct = Product(
+      id: Random().nextDouble().toString(),
+      title: _formData['name'] as String,
+      description: _formData['description'] as String,
+      price: _formData['price'] as double,
+      imageUrl: _formData['imageUrl'] as String,
+    );
+
+    print(newProduct.title);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Adicionar Produto'), centerTitle: true),
+      appBar: AppBar(
+        title: const Text('Adicionar Produto'),
+        centerTitle: true,
+        actions: [IconButton(onPressed: _formSave, icon: Icon(Icons.save))],
+      ),
       body: Padding(
         padding: EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: ListView(
             children: [
               TextFormField(
@@ -31,6 +81,16 @@ class _ProductFormState extends State<ProductForm> {
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocus);
+                },
+                onSaved: (name) => _formData['name'] = name ?? "",
+                validator: (_name) {
+                  final name = _name ?? "";
+
+                  if (name.trim().isEmpty) {
+                    return "Nome é obrigatório";
+                  }
+
+                  return null;
                 },
               ),
               TextFormField(
@@ -41,6 +101,16 @@ class _ProductFormState extends State<ProductForm> {
                   FocusScope.of(context).requestFocus(_descriptionFocus);
                 },
                 keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSaved: (price) =>
+                    _formData['price'] = double.parse(price ?? "0"),
+                validator: (_price) {
+                  final priceString = _price ?? "";
+                  final price = double.tryParse(priceString) ?? -1;
+                  if (price <= 0) {
+                    return "Preço inválido";
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 focusNode: _descriptionFocus,
@@ -48,6 +118,60 @@ class _ProductFormState extends State<ProductForm> {
                 textInputAction: TextInputAction.next,
                 keyboardType: TextInputType.multiline,
                 maxLines: 3,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_imageUrlFocus);
+                },
+                onSaved: (description) =>
+                    _formData['description'] = description ?? "",
+                validator: (_description) {
+                  final description = _description ?? "";
+
+                  if (description.trim().isEmpty) {
+                    return "Descrição é obrigatória";
+                  }
+
+                  return null;
+                },
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      focusNode: _imageUrlFocus,
+                      decoration: InputDecoration(labelText: "Url da Imagem"),
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.url,
+                      controller: _imageUrlController,
+                      onFieldSubmitted: (_) => _formSave(),
+                      onSaved: (imageUrl) =>
+                          _formData['imageUrl'] = imageUrl ?? "",
+                      validator: (_imageUrl) {
+                        final imageUrl = _imageUrl ?? "";
+
+                        if (!isValidImageUrl(imageUrl)) {
+                          return "Imagem inválida. [PNG,JPEG]";
+                        }
+
+                        return null;
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    margin: EdgeInsets.only(top: 16, left: 16),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                    ),
+                    alignment: Alignment.center,
+                    child: _imageUrlController.text.isEmpty
+                        ? Text("Informe a URL")
+                        : FittedBox(
+                            child: Image.network(_imageUrlController.text),
+                          ),
+                  ),
+                ],
               ),
             ],
           ),
